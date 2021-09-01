88602b276041c7dfb52645d35136d6e8cb3bce6c
Heitor Amaral <heitor.amaral90@outlook.com>
2021-09-01 19:56:58 -0300

    Métodos de busca geral
    Método findAll e findAllPaginated.

--- src/main/java/com/beerhouse/craftbeer/controller/BeerController.java
+++ src/main/java/com/beerhouse/craftbeer/controller/BeerController.java
@@ -1,9 +1,16 @@
 package com.beerhouse.craftbeer.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.beerhouse.craftbeer.domain.Beer;
 import com.beerhouse.craftbeer.service.BeerService;
 
 @RestController
@@ -13,4 +20,15 @@ public class BeerController {
 	@Autowired
 	private BeerService beerService;
 
+	@RequestMapping(method = RequestMethod.GET)
+	public ResponseEntity<List<Beer>> findAll() {
+		return ResponseEntity.ok().body(beerService.findAll());
+	}
+
+	@RequestMapping(method = RequestMethod.GET, path = "/paginated")
+	public ResponseEntity<Page<Beer>> findAllPaginated(@RequestParam(defaultValue = "0") Integer page,
+			@RequestParam(defaultValue = "24") Integer linesPerPage,
+			@RequestParam(defaultValue = "ASC") String direction, @RequestParam(defaultValue = "id") String orderBy) {
+		return ResponseEntity.ok().body(beerService.findAllPaginated(page, linesPerPage, direction, orderBy));
+	}
 }
--- src/main/java/com/beerhouse/craftbeer/service/BeerService.java
+++ src/main/java/com/beerhouse/craftbeer/service/BeerService.java
@@ -1,8 +1,14 @@
 package com.beerhouse.craftbeer.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
+import com.beerhouse.craftbeer.domain.Beer;
 import com.beerhouse.craftbeer.repository.BeerRepository;
 
 @Service
@@ -11,4 +17,12 @@ public class BeerService {
 	@Autowired
 	private BeerRepository beerRepository;
 
+	public List<Beer> findAll() {
+		return beerRepository.findAll();
+	}
+
+	public Page<Beer> findAllPaginated(Integer page, Integer linesPerPage, String direction, String orderBy) {
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		return beerRepository.findAll(pageRequest);
+	}
 }
